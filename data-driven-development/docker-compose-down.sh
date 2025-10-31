e3b512ca1ada9945e59b2f4f2abebc4fe8cbb163
kallenberg <oliver.kallenberg@ika.rwth-aachen.de>
2025-10-31 11:13:54 +0100

    Add some utils for debugging

---
+++ data-driven-development/docker-compose-down.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker rm data-driven-development-rosbag-record-1 data-driven-development-carla-simulation-controller-1 data-driven-development-time-health-check-1 data-driven-development-carla-ros-bridge-1 data-driven-development-spawned-vehicle-check-1 data-driven-development-carla-server-1 data-driven-development-carla-scenario-runner-1
